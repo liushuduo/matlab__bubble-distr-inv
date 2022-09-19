7c98aa1fbd40d10313c8e39ea5e4efb5b059545f
Shuduo Liu <sliu35@zju.edu.cn>
2022-09-19 22:15:11 +0800

    update figures

--- figures.m
+++ figures.m
@@ -1,25 +1,24 @@
 %% from N(a) to N
 clear; close all; clc
-palette = GetPalette();
-palette = palette.uchicago;
+palette = colorpalette("ieee_foundation");
 
 % the N(a) is a function where only one or zero exists
 load bubbles_exp.mat
 fH = figure(Position=[1800 -200 1000 500]);
 tH = tiledlayout(1,1, 'TileSpacing','tight', Padding='tight');
-tH.Units = "centimeters";
-tH.OuterPosition = [0 0 8.6 5.4] * 0.95;
+tH.Units = "inches";
+tH.OuterPosition = [0 0 3.5 3.5/16 * 9];
 aH = nexttile;
 sH = stem(aH, bubRadList*1e3, ones(size(bubRadList)));
-sH.Color = palette{5};
+sH.Color = palette{1};
 sH.Marker = "none";
-xlabel('bubble radius')
+xlabel('bubble radius (mm)')
 ylabel('$N(a)$', 'Interpreter','latex')
 ylim([0 1.5])
-aH.FontName = 'Arial';
-aH.FontSize = 12;
-aH.YTick = [];   aH.XTick = [];
-% exportgraphics(fH, './figures/Na.png', 'Resolution', 600);
+grid on
+aH.FontName = 'Times';
+aH.FontSize = 8;
+exportgraphics(fH, './figures/Na.png', 'Resolution', 600);
 
 % convert N(a) to vector N
 nBin = 31;
@@ -29,16 +28,17 @@ N = histcounts(bubRadList, binEdge);
 % plot the histogram
 fH = figure(Position=[1800 -200 1000 500]);
 tH = tiledlayout(1,1, 'TileSpacing','tight', Padding='tight');
-tH.Units = "centimeters";
-tH.OuterPosition = [0 0 8.6 5.4] * 0.95;
+tH.Units = "inches";
+tH.OuterPosition = [0 0 3.5 3.5/16*9];
 aH = nexttile;
-bH = bar(binEdge(1:end-1), N);
+bH = bar(binEdge(1:end-1) * 1e3, N);
+aH.YAxis.Exponent = 3;
 bH.EdgeColor = "none";
-bH.FaceColor = palette{5};
-aH.FontName = 'Arial';
-aH.FontSize = 12;
-aH.YTick = [];   aH.XTick = [];
-xlabel('bubble radius')
+bH.FaceColor = palette{1};
+aH.FontName = 'Times';
+aH.FontSize = 8;
+grid on
+xlabel('bubble radius (mm)')
 ylabel('$\mathbf{n}$', 'Interpreter','latex');
 exportgraphics(fH, './figures/vecN.png', 'Resolution', 600);
 
